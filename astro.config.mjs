582e5341c816c8f010f21d94a194bef68cf7f10f
Ryan Hunt <ryanahunt@outlook.com>
2025-11-11 22:51:35 -0600

    build check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	output: "hybrid", // Enable hybrid mode for API routes while keeping pages static
+	adapter: vercel(),
 });
